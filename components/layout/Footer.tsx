db3276f24fb0507c4c1c0460e9929f0ff2961da7
Harshil Patel <1080patelharshil@gmail.com>
2026-07-02 19:49:57 +0530

    chore: add official YouTube channel URL in the layout footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -28,7 +28,7 @@ const socialLinks = [
   },
   {
     label: "YouTube",
-    href: "#",
+    href: "https://www.youtube.com/@BhumiDevelopers1995",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
         <path d="M22.54 6.42a2.78 2.78 0 0 0-1.95-1.96C18.88 4 12 4 12 4s-6.88 0-8.59.46A2.78 2.78 0 0 0 1.46 6.42 29 29 0 0 0 1 12a29 29 0 0 0 .46 5.58A2.78 2.78 0 0 0 3.41 19.6C5.12 20 12 20 12 20s6.88 0 8.59-.4a2.78 2.78 0 0 0 1.95-1.96A29 29 0 0 0 23 12a29 29 0 0 0-.46-5.58zM9.75 15.02V8.98L15.5 12l-5.75 3.02z" />
